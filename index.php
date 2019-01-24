0eacbe8273d0a274f18a275fd262faeb2757940e
Farhoe <matej.dalekorej@student.ossp.cz>
2019-01-24 08:29:41 +0100

    hehehe :)

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body> 
+    <?php
+    //for ($ahojda=0; $ahojda < 100; $ahojda++) {
+    // echo $ahojda + 1 . "<br> <strong> <em>";
+    //}
+
+    //for ($cau=0; $cau < 100; $cau=$cau+5) {
+    //echo $cau . "<br>";
+    //}
+
+    //for ($index = 100; $index > 0 ; $index=$index-1) {
+      //echo $index  . "<br>";
+    //}
+
+     ?>
+
+
+  </body>
+</html>
